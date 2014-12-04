e3461f8edff485de13d0a3e8db89d8f384a84524
Rosen Stoyanov <me@pockata.org>
2014-12-05 01:51:29 +0200

    Image: added test for local image loading

--- test/image.js
+++ test/image.js
@@ -3,6 +3,8 @@
 
 var expect = require('chai').expect;
 var Shape  = require('../lib/shape');
+var fs     = require('fs');
+var bEqual = require('buffer-equal');
 var Img    = require('../lib/shapes/image');
 
 describe('Image shape', function () {
@@ -33,6 +35,38 @@ describe('Image shape', function () {
         it('should set getters/setters for new instance  properties', function () {
             expect(image.x).to.exist;
             expect(image.opacity).to.exist;
+        });
+    });
+
+    describe('image loading', function () {
+
+        var fName = '/tmp/test.png';
+
+        before(function (done) {
+            var testImage = 'R0lGODlhEwALAKECAAAAABISEv///////yH5BAEKAAIALAAAAAATAAsAAAIdDI6pZ+suQJyy0ocV3bbm33EcCArmiUYk1qxAUAAAOw==';
+            fs.writeFile(fName, testImage, 'base64', done);
+        });
+
+        it('should load local image properly', function (done) {
+
+            var loader = image.load(fName);
+            loader.fail(done);
+
+            loader.then(function () {
+
+                fs.readFile(fName, function (err, data) {
+                    if (err) {
+                        done(err);
+                    }
+
+                    if (bEqual(image.Image.src, data)) {
+                        done();
+                    }
+                    else {
+                        done(new Error('Image sources don\'t match'));
+                    }
+                });
+            });
         });
     });
 });
